ea4b93f807ead68a7ddc218b992d3e1178cdb046
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-04-06 15:56:01 +0530

    :fire: Remove redundant code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import AboutUs from "./components/AboutUs";
-import AnxietyPage from "./components/AnxietyPage";
-import Blogs from "./components/Blogs";
-import CareerCounselling from "./components/CareerCounselling";
-import ContactUs from "./components/ContactUs";
-import HealMyMind from "./components/HealMyMind";
-import IndividualCounselling from "./components/IndividualCounselling";
-import MainPage from "./components/MainPage";
-import TherapeauticAudios from "./components/TherapeauticAudios";
 import HomePage from "./pages/HomePage";
 
 function App() {
